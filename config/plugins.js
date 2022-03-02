01d0199cda3cd946548b07aec01055f9c151dcae
Thomas Thirion <thirionthomas68100@gmail.com>
2022-03-02 15:48:37 +0100

    2eme commit

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('thomas-thirion'),
-        api_key: env('784933485352314'),
-        api_secret: env('da6GUP6Cp2lRmr9xnbPc1pS8Hn4'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_API_KEY'),
+        api_secret: env('CLOUDINARY_API_SECRET'),
       },
     },    
   });

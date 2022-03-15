b7ffeabf91ba2983a28eff4da6fcbd6ff92afbb9
Thomas Thirion <thirionthomas68100@gmail.com>
2022-03-15 21:04:51 +0100

    modif plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,20 @@
-module.exports = ({ env }) => ({
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_API_KEY'),
-        api_secret: env('CLOUDINARY_API_SECRET'),
-      },
-    },    
+module.exports = ({ env }) => {
+
+  // TODo -> delete afterwards
+  console.log('Cloudinary Information ->', {
+    cloud_name: env("CLOUDINARY_NAME"),
+    api_key: env("CLOUDINARY_API_KEY"),
+    api_secret: env("CLOUDINARY_API_SECRET"),
   });
+
+  return ({
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_API_KEY"),
+        api_secret: env("CLOUDINARY_API_SECRET"),
+      },
+    },
+  });
+};

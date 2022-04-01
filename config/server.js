23a1249201cea22aba1a203ab47a34b560f20019
Thomas Thirion <thirionthomas68100@gmail.com>
2022-04-01 12:56:23 +0200

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', 'd1838bb10cede4205ad9420ae59ed191'),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd1838bb10cede4205ad9420ae59ed191'),
-    },
-  },
+  url: env("MY_HEROKU_URL"),
 });

35862c6469b18e12e05e1d1f24dc6da0b3ada693
Thomas Thirion <thirionthomas68100@gmail.com>
2022-05-05 11:57:48 +0200

    modif plugin.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,37 @@
-module.exports = ({ env }) => {
+// module.exports = ({ env }) => {
 
-  // TODo -> delete afterwards
-  console.log('Cloudinary Information ->', {
-    cloud_name: env("CLOUDINARY_NAME"),
-    api_key: env("CLOUDINARY_API_KEY"),
-    api_secret: env("CLOUDINARY_API_SECRET"),
-  });
+// TODo -> delete afterwards
+// console.log('Cloudinary Information ->', {
+//   cloud_name: env("CLOUDINARY_NAME"),
+//   api_key: env("CLOUDINARY_API_KEY"),
+//   api_secret: env("CLOUDINARY_API_SECRET"),
+// });
 
-  return ({
-    upload: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_API_KEY"),
-        api_secret: env("CLOUDINARY_API_SECRET"),
-      },
+// return ({
+//     upload: {
+//       provider: "cloudinary",
+//       providerOptions: {
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_API_KEY"),
+//         api_secret: env("CLOUDINARY_API_SECRET"),
+//       },
+//     },
+//   });
+// };
+
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
-  });
-};
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+  // ...
+});
